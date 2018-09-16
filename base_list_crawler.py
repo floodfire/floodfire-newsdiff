e9c4d2b5476ff5cd247ef77eead2051bcc250445
Tuvix Shih <tuvix@ninthday.info>
2018-09-16 22:18:21 +0800

    新增基礎的抽象類別
    新增 list 和 page 頁面爬蟲的基礎檔案。安裝 requests 套件。

---
+++ base_list_crawler.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from abc import ABCMeta, abstractmethod
+
+class BaseListCrawler(metaclass=ABCMeta):
+    _url = ''
+
+    @property
+    @abstractmethod
+    def url(self):
+        return self._url
+
+    @url.setter
+    @abstractmethod
+    def url(self, value):
+        self._url = value
+
+    @abstractmethod
+    def fetch_html(self, url):
+        """
+        傳回 List 頁面的 HTML
+        """
+        return NotImplemented
+    
+    @abstractmethod
+    def fetch_list(self, soup):
+        """
+        傳回 HTML 中所有的 List
+        """
+        return NotImplemented
+
+    @abstractmethod
+    def count_page(self):
+        """
+        傳回 List 的總頁數
+        """
+        return NotImplemented
