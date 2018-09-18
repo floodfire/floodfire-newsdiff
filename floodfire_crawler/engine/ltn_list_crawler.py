2356b7dcdb05c02f741fad08b60462e2cc4698f4
Tuvix Shih <tuvix@ninthday.info>
2018-09-18 18:18:07 +0800

    增加獲取list內容的方法

--- floodfire_crawler/engine/ltn_list_crawler.py
+++ floodfire_crawler/engine/ltn_list_crawler.py
@@ -2,6 +2,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+from hashlib import md5
 from floodfire_crawler.core.base_list_crawler import BaseListCrawler
 
 class LtnListCrawler(BaseListCrawler):
@@ -23,19 +24,31 @@ class LtnListCrawler(BaseListCrawler):
 
     def fetch_list(self, soup):
         news = []
-        news_lis = soup.find('ul', 'imm').find_all('li')
-        for news_li in news_lis:
-            link_a = news_li.find('a', class_='tit')
+        news_rows = soup.find('ul', 'imm').find_all('li')
+        md5hash = md5()
+        for news_row in news_rows:
+            link_a = news_row.find('a', class_='tit')
+            md5hash.update(link_a['href'].encode('utf-8'))
             raw = {
                 'title': link_a.p.text.strip(),
-                'href': link_a['href']
+                'url': link_a['href'],
+                'url_md5': md5hash.hexdigest(),
+                'source_id': 5,
+                'category': self.get_category(news_row)
             }
             news.append(raw)
         return news
 
     def count_page(self):
         pass
-    
+
+    def get_category(self, news_row):
+        cate_list = []
+        categories = news_row.find('div', class_='tagarea').find_all('a')
+        for category in categories:
+            cate_list.append(category.text)
+        return ','.join(cate_list)
+
     def run(self):
         html = self.fetch_html()
         soup = BeautifulSoup(html, 'html.parser')
