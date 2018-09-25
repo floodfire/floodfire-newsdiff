344e19291a5121b21efe0929ae74b4166f9e9304
Tuvix Shih <tuvix@ninthday.info>
2018-09-25 21:45:50 +0800

    增加遍歷各頁方式及檢查連續重複就跳出
    - 新增一頁一頁 list 爬梳函式
    - 若連續檢查都已存在資料庫達一定數量就停止往下爬已節省資源

--- floodfire_crawler/engine/ltn_list_crawler.py
+++ floodfire_crawler/engine/ltn_list_crawler.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 from hashlib import md5
+from time import sleep
 from floodfire_crawler.core.base_list_crawler import BaseListCrawler
 from floodfire_crawler.storage.rdb_storage import FloodfireStorage
 
@@ -19,8 +20,8 @@ class LtnListCrawler(BaseListCrawler):
     def __init__(self, config):
         self.floodfire_storage = FloodfireStorage(config)
 
-    def fetch_html(self):
-        req = requests.get(self.url)
+    def fetch_html(self, url):
+        req = requests.get(url)
 
         if req.status_code == requests.codes.ok:
             html = req.text
@@ -29,14 +30,14 @@ class LtnListCrawler(BaseListCrawler):
     def fetch_list(self, soup):
         news = []
         news_rows = soup.find('ul', 'imm').find_all('li')
-        md5hash = md5()
+        #md5hash = md5()
         for news_row in news_rows:
             link_a = news_row.find('a', class_='tit')
-            md5hash.update(link_a['href'].encode('utf-8'))
+            md5hash = md5(link_a['href'].encode('utf-8')).hexdigest()
             raw = {
                 'title': link_a.p.text.strip(),
                 'url': link_a['href'],
-                'url_md5': md5hash.hexdigest(),
+                'url_md5': md5hash,
                 'source_id': 5,
                 'category': self.get_category(news_row)
             }
@@ -56,9 +57,34 @@ class LtnListCrawler(BaseListCrawler):
             cate_list.append(category.text)
         return ','.join(cate_list)
 
+    def make_a_round(self, start_page, end_page):
+        consecutive = 0
+        for page in range(start_page, end_page+1):
+            if consecutive > 20:
+                print('News consecutive more than 20, stop crawler!!')
+                break
+            page_url = self.url + '/all/' + str(page)
+            print(page_url)
+            sleep(2)
+            html = self.fetch_html(page_url)
+            soup = BeautifulSoup(html, 'html.parser')
+            news_list = self.fetch_list(soup)
+            #print(news_list)
+            for news in news_list:
+                if(self.floodfire_storage.check_list(news['url_md5']) == 0):
+                    self.floodfire_storage.insert_list(news)
+                    consecutive = 0
+                else:
+                    print(news['title']+' exist! skip insert.')
+                    consecutive += 1
+
+
     def run(self):
-        html = self.fetch_html()
+        html = self.fetch_html(self.url)
         soup = BeautifulSoup(html, 'html.parser')
+        last_page = self.get_last(soup)
+        self.make_a_round(1, last_page)
+        """
         news_list = self.fetch_list(soup)
         print(news_list)
         for news in news_list:
@@ -69,5 +95,5 @@ class LtnListCrawler(BaseListCrawler):
             
         last_page = self.get_last(soup)
         print(last_page)
-
+        """
         
