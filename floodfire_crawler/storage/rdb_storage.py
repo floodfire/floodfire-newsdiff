33fc88a32d658ea50f9b83913d66646fa856e795
Tuvix Shih <tuvix@ninthday.info>
2018-09-30 14:31:16 +0800

    移除 base list class 中的 get_last方法
    由於不是所有的媒體 list 中都包含有最後一頁，將抽象類別中的 get_last
    方法移除。
    
    - 移除 get_last 抽象方法
    - 新增 run 抽象方法
    - 新增 rdb_storage 中的說明

--- floodfire_crawler/storage/rdb_storage.py
+++ floodfire_crawler/storage/rdb_storage.py
@@ -35,6 +35,15 @@ class FloodfireStorage():
             print('Error! Insert new list error!')
 
     def check_list(self, url_hash):
+        """
+        檢查 list 是否已經存在
+
+        Keyword arguments:
+            url_hash (string) -- list 代表的 hansh 值
+
+        Returns:
+            url_hash 查詢到的筆數
+        """
         sql = "SELECT * FROM `list` WHERE `url_md5`=%s"
         try:
             self.cur.execute(sql, (url_hash,))
@@ -44,12 +53,51 @@ class FloodfireStorage():
             print('Error! Insert new list error!')
         return row_count
 
+    def update_list_crawlercount(self, url_hash):
+        """
+        更新列表抓取次數
+
+        Keyword arguments:
+            url_hash (string) -- list 代表的 hansh 值
+        """
+        pass
+    
     def get_source_list(self):
+        """
+        取得所有媒體列表
+
+        Returns:
+            所有系統中的媒體清單 dictionary
+        """
         pass
 
     def get_source_id(self, code_name):
+        """
+        由代號取得 media source 編號
+
+        Keyword arguments:
+            code_name (string) -- 媒體簡寫英文名稱
+        """
         pass
 
+    def insert_page(self, page_row):
+        """
+        新增 news page 資料
+
+        Keyword arguments:
+            page (dictionary) -- 新聞 page 的新聞內容
+        """
+        pass
+
+    def insert_page_raw(self, html_raw):
+        """
+        新增 news page 的原始 html 資料
+
+        Keyword arguments:
+            html_raw (string) -- 新聞頁原始 HTML 資料內容
+        """
+        pass
+    
     def __del__(self):
         self.cur.close()
         self.conn.close()
