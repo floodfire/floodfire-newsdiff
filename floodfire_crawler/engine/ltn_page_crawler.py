748d20454c03b80151a45a580f1d4bbce4ace0d8
Tuvix Shih <tuvix@ninthday.info>
2018-10-02 16:47:30 +0800

    新增自由時報抓取類別及補充說明
    - 加入玩咖類別網頁內容抓取與解析
    - 補充 LtnPageCrawler 方法說明
    - LtnPageCrawler fetch_html 方法加入 timeout 設定
    - LtnPageCrawler 檢查抓取 http status
    - LtnPageCrawler 沒一筆資料抓取時間間隔 2~6 秒隨機
    - FloodfireStorage 新增更新 error count 方法
    - FloodfireStorage 抓取 crawl list 擴增至一次 50 筆

--- floodfire_crawler/engine/ltn_page_crawler.py
+++ floodfire_crawler/engine/ltn_page_crawler.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from time import sleep, strftime, strptime
+from random import randint
 from floodfire_crawler.core.base_page_crawler import BasePageCrawler
 from floodfire_crawler.storage.rdb_storage import FloodfireStorage
 
@@ -14,16 +15,35 @@ class LtnPageCrawler(BasePageCrawler):
         self.floodfire_storage = FloodfireStorage(config)
 
     def fetch_html(self, url):
-        response = requests.get(url)
+        """
+        取出網頁 HTML 原始碼
 
-        if response.status_code == requests.codes.ok:
-            html = response.text
-        
-        # 取得最後 redirect 之後的真實網址
-        last_url = response.url
-        return last_url, html
+        Keyword arguments:
+            url (string) -- 抓取的網頁網址
+        """
+        try:
+            response = requests.get(url, timeout=15)
+            resp_content = {
+                'redirected_url': response.url, # 取得最後 redirect 之後的真實網址
+                'html': response.text
+            }
+        except requests.exceptions.HTTPError as err:
+            msg = "HTTP exception error: {}".format(err)
+            return 0, msg
+        except requests.exceptions.RequestException as e:
+            msg = "Exception error {}".format(e)
+            return 0, msg
+
+        return response.status_code, resp_content
 
     def fetch_news_content(self, category, soup):
+        """
+        取出網頁中的新聞內容
+
+        Keyword arguments:
+            category (string) -- 類別
+            soup (beautifulsoup) -- 已經 parse 過的 BeautifulSoup 物件
+        """
         news_page = {}
         if category == 'news':
             article_title = soup.find('div', class_='whitecon articlebody')
@@ -87,9 +107,23 @@ class LtnPageCrawler(BasePageCrawler):
             p_tags = article_content.find_all('p',recursive=False)
             news_page['body'] = "\n".join([p.text for p in p_tags])
             news_page['publish_time'] = self.fetch_publish_time(category, article_content)
+        elif category == 'playing':
+            article_title = soup.find('div', class_='article_header')
+            news_page['title'] = article_title.h1.text.strip()
+            article_content = soup.find('div', itemprop='articleBody').find('div', class_='text')
+            p_tags = article_content.find_all('p',recursive=False)
+            news_page['body'] = "\n".join([p.text.strip() for p in p_tags])
+            news_page['publish_time'] = self.fetch_publish_time(category, soup)
         return news_page
 
     def fetch_publish_time(self, category, soup):
+        """
+        取出網頁中的發佈時間
+
+        Keyword arguments:
+            category (string) -- 類別
+            soup (beautifulsoup) -- 已經 parse 過的 BeautifulSoup 物件
+        """
         publish_time = ''
         if category == 'news':
             publish_time = soup.find('span', class_='viewtime').text
@@ -110,9 +144,18 @@ class LtnPageCrawler(BasePageCrawler):
             publish_time = soup.find('span', class_='date1').text
         elif category == 'auto':
             publish_time = soup.find('span', class_='h1dt').text
+        elif category == 'playing':
+            time_string = soup.find('meta', property='article:published_time')
+            publish_time = strftime('%Y-%m-%d %H:%M:%S', strptime(time_string['content'][:-7], '%Y-%m-%dT%H:%M:%S'))
         return publish_time
     
     def extract_type(self, url):
+        """
+        萃取網址 domain 第一段作為類別
+
+        Keyword arguments:
+            url (string) -- 要進行萃取的網址
+        """
         hostname = urlparse(url).hostname
         page_type = hostname.split('.')[0]
         return page_type
@@ -122,34 +165,39 @@ class LtnPageCrawler(BasePageCrawler):
         """
         程式進入點
         """
-        # crawl_category = ['news', 'ent', 'ec', 'sports']
         source_id = self.floodfire_storage.get_source_id(self.code_name)
         crawl_list = self.floodfire_storage.get_crawllist(source_id)
+        
         for row in crawl_list:
-            redirected_url, html = self.fetch_html(row['url'])
-            print(row['url'])
-            print(redirected_url)
-            page_type = self.extract_type(redirected_url)
-            # if page_type not in crawl_category:
-            #     continue
-            soup = BeautifulSoup(html, 'html.parser')
-            news_page = self.fetch_news_content(page_type, soup)
-            print(news_page)
-            news_page['list_id'] = row['id']
-            news_page['url'] = row['url']
-            news_page['url_md5'] = row['url_md5']
-            news_page['redirected_url'] = redirected_url
-            news_page['source_id'] = source_id
-            news_page['image'] = 0
-            news_page['video'] = 0
+            status_code, html_content = self.fetch_html(row['url'])
+            if status_code == requests.codes.ok:
+                page_type = self.extract_type(html_content['redirected_url'])
 
-            self.floodfire_storage.insert_page(news_page)
-            # 更新爬抓次數記錄
-            self.floodfire_storage.update_list_crawlercount(row['url_md5'])
-            sleep(2)
+                soup = BeautifulSoup(html_content['html'], 'html.parser')
+                news_page = self.fetch_news_content(page_type, soup)
+                # print(news_page)
+                news_page['list_id'] = row['id']
+                news_page['url'] = row['url']
+                news_page['url_md5'] = row['url_md5']
+                news_page['redirected_url'] = html_content['redirected_url']
+                news_page['source_id'] = source_id
+                news_page['image'] = 0
+                news_page['video'] = 0
 
-        # redirected_url, html = self.fetch_html('http://auto.ltn.com.tw/news/10906/3')
-        # page_type = self.extract_type(redirected_url)
-        # soup = BeautifulSoup(html, 'html.parser')
-        # news_page = self.fetch_news_content(page_type, soup)
-        # print(news_page)
+                self.floodfire_storage.insert_page(news_page)
+                # 更新爬抓次數記錄
+                self.floodfire_storage.update_list_crawlercount(row['url_md5'])
+                # 隨機睡 2~6 秒再進入下一筆抓取
+                print('crawling...id: {}'.format(row['id']))
+                sleep(randint(2, 6))
+            else:
+                # get 網頁失敗的時候更新 error count
+                self.floodfire_storage.update_list_errorcount(row['url_md5'])
+
+        # 單頁測試
+        # status_code, html_content = self.fetch_html('http://playing.ltn.com.tw/article/10754')
+        # if status_code == requests.codes.ok:
+        #     page_type = self.extract_type(html_content['redirected_url'])
+        #     soup = BeautifulSoup(html_content['html'], 'html.parser')
+        #     news_page = self.fetch_news_content(page_type, soup)
+        #     print(news_page)
