8d6eed48b8e251b383cb3a10bf4b343369883af9
Tuvix Shih <tuvix@ninthday.info>
2018-11-06 23:00:44 +0800

    新增中時電子報新聞頁面抓取內文、發佈時間、關鍵字、記者

--- floodfire_crawler/engine/cnt_page_crawler.py
+++ floodfire_crawler/engine/cnt_page_crawler.py
@@ -45,13 +45,74 @@ class CntPageCrawler(BasePageCrawler):
         return response.status_code, resp_content
 
     def fetch_news_content(self, soup):
+        """
+        傳回新聞頁面中的內容
+
+        keyward arguments:
+            soup (object) -- beautifulsoup object
+        """
         page = dict()
-        title = soup.select('div#bigpicbox h1#h1')[0].text.strip()
-        # content = soup.select('div.contentbox div.left div.clummbox')
-        print(title)
+        # --- 取出標題 ---
+        page['title'] = soup.select('div#bigpicbox h1#h1')[0].text.strip()
+        # --- 取出內文 ---
+        article_content = soup.select('article.arttext')[0]
+        p_tags = article_content.find_all('p')
+        page['body'] = "\n".join([p.text for p in p_tags])
+
+        # --- 取出發布時間 ---
+        page['publish_time'] = self.fetch_publish_time(article_content)
+        
+        # --- 取出記者 ---
+        page['authors'] = article_content.find('div', class_='rp_name').find('cite').text.strip()
+        
+        # --- 取出關鍵字 ---
+        keywords = soup.find('meta', attrs={'name':'news_keywords'})
+        page['keywords'] = keywords['content'].strip()
+
+        # -- 取出視覺資料連結（圖片） ---
+        page['visual_contents'] = list()
+        if soup.find('div', class_='picbox1'):
+            cover_img=soup.find('div', class_='picbox1')
+            page['visual_contents'].append(
+                {
+                    'type': 1,
+                    'visual_src': 'https:' + cover_img.find('img')['src'],
+                    'caption': cover_img.find('a')['title']
+                })
+        imgs = article_content.find_all('div', class_='picbox')
+        for img in imgs:
+            page['visual_contents'].append(
+                {
+                    'type': 1,
+                    'visual_src': 'https:' + img.find('img')['src'],
+                    'caption': img.find('span').text.strip()
+                })
+
+        # -- 取出視覺資料連結（影片） ---
+        if soup.find('div', class_='video'):
+            video = soup.find('div', class_='video')
+            if video.find('div', id='jw-container'):
+                print(video.find('div', id='jw-container'))
+                # page['visual_contents'].append(
+                # {
+                #     'type': 2,
+                #     'visual_src': video.find('div', id='jw-container').find('iframe')['src'],
+                #     'caption': video.find('div', id='jw-container').find('figcaption').text.strip()
+                # })
+        print(soup.find_all('div', class_='video_iframe'))
+        
+        # print(page)
 
     def fetch_publish_time(self, soup):
-        pass
+        """
+        取得新聞發佈時間
+
+        keyward arguments:
+            soup (object) -- beautifulsoup object
+        """
+        time = soup.find('time').text.strip()
+        news_time = strftime('%Y-%m-%d %H:%M:%S', strptime(time, '%Y年%m月%d日 %H:%M'))
+        return news_time
 
     def run(self, page_raw=False, page_diff=False, page_visual=False):
         """
@@ -72,7 +133,7 @@ class CntPageCrawler(BasePageCrawler):
         crawl_count = 0
 
         # 單頁測試
-        status_code, html_content = self.fetch_html('https://www.chinatimes.com/realtimenews/20181029003657-260407')
+        status_code, html_content = self.fetch_html('https://www.chinatimes.com/realtimenews/20181106002002-260407')
         if status_code == requests.codes.ok:
             # page_type = self.extract_type(html_content['redirected_url'])
             soup = BeautifulSoup(html_content['html'], 'html.parser')
