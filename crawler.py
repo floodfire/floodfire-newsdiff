ad49b7f768a1dfa9c00ac17475459533e2896601
Chaofeng <jimy19921111@gmail.com>
2020-05-19 23:43:03 +0800

    蘋果日報檢修：list, page
    由於蘋果日報整體頁面改版，因此重新修改
    目前蘋果日報取得內容皆透過API

--- crawler.py
+++ crawler.py
@@ -59,7 +59,7 @@ class Crawler():
         """
         if self.args.typeof == 'list':
             alc = ApdListCrawler(self.config)
-            alc.url = 'https://tw.appledaily.com/new'
+            alc.url = 'https://tw.appledaily.com/realtime/new/'
             alc.run()
         elif self.args.typeof == 'page':
             apc = ApdPageCrawler(self.config, self.logme)
--- floodfire_crawler/engine/apd_list_crawler.py
+++ floodfire_crawler/engine/apd_list_crawler.py
@@ -6,6 +6,7 @@ from hashlib import md5
 from time import sleep
 from floodfire_crawler.core.base_list_crawler import BaseListCrawler
 from floodfire_crawler.storage.rdb_storage import FloodfireStorage
+import json
 
 class ApdListCrawler(BaseListCrawler):
 
@@ -28,65 +29,49 @@ class ApdListCrawler(BaseListCrawler):
         html = response.text
         return html
 
-
-        if req.status_code == requests.codes.ok:
-            html = req.text
-        return html
-
     def get_last(self):
-        return null
+        return None
 	
 	
-    def fetch_list(self, soup):
+    def fetch_list(self, json_soup):
         news = []
-        news_rows = soup.find_all("li", {"class": "rtddt"})
+        news_rows = json_soup['content_elements']
         #md5hash = md5()
         for news_row in news_rows:
-            link_a = news_row.find('a')
-            md5hash = md5(link_a['href'].encode('utf-8')).hexdigest()
+            link_a = 'https://tw.appledaily.com'+news_row['websites']['tw-appledaily']['website_url']
+            md5hash = md5(link_a.encode('utf-8')).hexdigest()
             raw = {
-                'title': link_a.font.text.strip().replace("　"," ").replace("\u200b",""),
-                'url': link_a['href'],
+                'title': news_row['headlines']['basic'].replace('\u3000', '　'),
+                'url': link_a,
                 'url_md5': md5hash,
                 'source_id': 1,
-                'category': link_a.h2.text
+                'category': news_row['taxonomy']['primary_section']['name']
             }
             news.append(raw)
         return news
 
     def make_a_round(self):
+        url = 'https://tw.appledaily.com/pf/api/v3/content/fetch/query-feed?query=%7B%22feedOffset%22%3A0%2C%22feedQuery%22%3A%22%22%2C%22feedSize%22%3A%22100%22%2C%22sort%22%3A%22display_date%3Adesc%22%7D&d=72&_website=tw-appledaily'
+        html = self.fetch_html(url)
+        json_soup = json.loads(html)
         consecutive = 0
-        start_page = 1
-        page = 1
-        while(1):
+
+        news_list = self.fetch_list(json_soup)
+        #print(news_list)
+        for news in news_list:
             if consecutive > 20:
                 print('News consecutive more than 20, stop crawler!!')
                 break
-            page_url = self.url + '/realtime/' + str(page)
-            print(page_url)
-            sleep(2)
-            html = self.fetch_html(page_url)
-            soup = BeautifulSoup(html, 'html.parser')
-            
-            if(soup.contents[0]!='html'):
-                break
-            
-            
-            news_list = self.fetch_list(soup)
-            #print(news_list)
-            for news in news_list:
-                if(self.floodfire_storage.check_list(news['url_md5']) == 0):
-                    self.floodfire_storage.insert_list(news)
-                    consecutive = 0
-                else:
-                    print(news['title']+' exist! skip insert.')
-                    consecutive += 1
-            page += 1
+
+            if(self.floodfire_storage.check_list(news['url_md5']) == 0):
+                self.floodfire_storage.insert_list(news)
+                consecutive = 0
+            else:
+                print(news['title']+' exist! skip insert.')
+                consecutive += 1
 
 
     def run(self):
-        html = self.fetch_html(self.url)
-        soup = BeautifulSoup(html, 'html.parser')
         self.make_a_round()
         """
         news_list = self.fetch_list(soup)
